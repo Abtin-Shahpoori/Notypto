82c207501d14426838e58d6cf8148d6ea5256c6e
abtin <abtin.shahpoori.monfared@gmail.com>
2021-09-23 23:02:06 +0330

    bug fixes

--- index.js
+++ index.js
@@ -12,9 +12,7 @@ const TransactionMiner = require('./app/transaction-miner');
 const { decrypt } = require('./util/encrypt_decrypt')
 
 const isDevelopment = process.env.ENV === 'development';
-console.log(process.env.ENV);
-
-const REDIS_URL = isDevelopment ?
+const REDIS_URL = isDevelopment?
 	'redis://127.0.0.1:6379': 
 	'redis://:pcb08107675285379633dd82d3fa9aefbfdeebe7ddd16b646c8caed4d1dfd40ca@ec2-34-239-208-3.compute-1.amazonaws.com:32219'
 
