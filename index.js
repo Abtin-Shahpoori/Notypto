04c94dd7b237763a1bec9b207a6291228c2e6cc1
Abtin Shahpoori <abtin.shahpoori.monfared@gmail.com>
2021-11-02 10:08:36 +0330

    fixed ports bug

--- index.js
+++ index.js
@@ -329,7 +329,7 @@ if(process.env.GENERATE_PEER_PORT == 'true') {
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 
-const PORT = PEER_PORT || DEFAULT_PORT || process.env.PORT; 
+const PORT = process.env.PORT || PEER_PORT || DEFAULT_PORT; 
 app.listen(PORT, () => {
     console.log(`listening at localhost:${PORT}`);
 
